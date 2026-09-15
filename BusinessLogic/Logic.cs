c6c68e8c21f5be5a0d7126f2c607986f2bfa5237
Anastasia Lepeshonok <nastlenns@gmail.com>
2026-09-15 14:06:43 +0700

    добавлен класс Logic

--- BusinessLogic/Logic.cs
+++ BusinessLogic/Logic.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace BusinessLogic
 {
-    internal class Logic
+    public class Logic
     {
+        public List<Student> students { get; set; } = new List<Student>();
+
+        public void AddStudent(string name, string speciality, string group)
+        {
+            students.Add(new Student
+            {
+                Name = name,
+                Speciality = speciality,
+                Group = group
+            });
+        }
+
+        public void DeleteStudent(string name, string speciality, string group)
+        {
+            var student = students.FirstOrDefault(s =>
+                s.Name == name &&
+                s.Speciality == speciality &&
+                s.Group == group);
+
+            if (student != null)
+                students.Remove(student);
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public Dictionary<string, int> GetSpecialityDistribution()
+        {
+            return students
+                .GroupBy(s => s.Speciality)
+                .ToDictionary(g => g.Key, g => g.Count());
+        }
     }
 }
